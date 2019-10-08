cee4195a87c939898464bbe50c12bfac4d39be1c
user <hcknbrgr@gmail.com>
2019-10-08 05:26:04 -0400

    finished remove/finished program

--- src/bst.hpp
+++ src/bst.hpp
@@ -97,6 +97,7 @@ template<class T>
 template<class T>
 void BST<T>::insert(T new_data)
 {
+    node_count++;
     root = insertHelper(new_data, root);
 }
 
@@ -130,7 +131,99 @@ Node<T> *BST<T>::search(T val)
 template<class T>
 void BST<T>::remove(T val)
 {
+    //3 general cases....node to remove has 0, 1, or 2 children
 
+
+    Node<T> *curNode = root;
+    Node<T> *parentNode = NULL;
+
+    while(curNode!= NULL)
+    {
+        if (val == curNode->get_data())
+        {
+
+            //2. if(node to remove has one node), set parent to deleting node's child to skip over the node to remove (set pointer to other node), delete  node
+            //3. if(node has 2 children...), find the largest item in the left subtree OR find the smallest item in the right subtree
+            //      then put that node in place of the one you want to remove
+            //if removal successful, node_count--;
+            if ((curNode->get_left()==NULL)&&(curNode->get_right()==NULL))
+            {
+                if (parentNode == NULL)
+                {
+                    root = NULL;
+                }
+                else if (parentNode->get_left() == curNode)
+                {
+                    parentNode->set_left(NULL);
+                }
+                else
+                {
+                    parentNode->set_right(NULL);
+                }
+                delete(curNode);
+                node_count--;
+            }
+            else if ((curNode->get_left()==NULL)&&(curNode->get_right() != NULL))//Remove node with only RIGHT child
+            {
+                if(parentNode==NULL)
+                {
+                    root = curNode->get_right();
+                }
+                else if (parentNode->get_left()== curNode)
+                {
+                    parentNode->set_left(curNode->get_right());
+                }
+                else
+                {
+                    parentNode->set_right(curNode->get_right());
+                }
+                delete(curNode);
+                node_count--;
+            }
+            else if ((curNode->get_left()!=NULL) && (curNode->get_right() == NULL))//Remove node with only LEFT child
+            {
+                if(parentNode==NULL)
+                {
+                    root = curNode->get_left();
+                }
+                else if (parentNode->get_left()== curNode)
+                {
+                    parentNode->set_left(curNode->get_left());
+                }
+                else
+                {
+                    parentNode->set_right(curNode->get_left());
+                }
+                delete(curNode);
+                node_count--;
+            }
+            else // the node has 2 children
+            {
+                Node<T> *successorNode = curNode->get_right();
+                T succData;
+                while (successorNode->get_left()!= NULL)
+                {
+                    successorNode=successorNode->get_left();
+                }
+                succData = successorNode->get_data();
+                remove(succData);
+                curNode->set_data(succData);
+            }
+        }
+        else if(curNode->get_data()>val) // search left for value to remove
+        {
+            parentNode = curNode;
+            curNode = curNode->get_left();
+        }
+        else // (curNode->get_data()<val)  search right for value to remove
+        {
+            parentNode = curNode;
+            curNode = curNode->get_right();
+        }
+    }
+    return;
+
+    
 }
 
 
@@ -138,6 +231,7 @@ void BST<T>::remove(T val)
 template<class T>
 int BST<T>::get_size()
 {
+    return node_count;
 
 }
 
